bd5e45d02b6b592d3854184ba2acfa8b3665a947
Nick Arocho <nick@boldlyfine.com>
2018-02-23 12:05:35 -0800

    provide JWT when user signs up

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,6 @@
 var User = require('../models/user');
+var jwt = require('jsonwebtoken');
+var SECRET = process.env.SECRET;
 
 module.exports = {
   signup
@@ -9,8 +11,16 @@ function signup(req, res) {
   user.save()
     .then(user => {
       // TODO: Send back a JWT instead of the user
-      res.json(user);
+      res.json({token: createJWT(user)});
     })
     // User data invalid
     .catch(err => res.status(400).json(err));
+}
+
+function createJWT(user) {
+  return jwt.sign(
+    {user},
+    SECRET,
+    {expiresIn: '24h'}
+  )
 }
